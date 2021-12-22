e8d838fa925972c008f2b7966c952b67e13511f0
AdanaSyaifullah <datebayo200116@gmail.com>
2021-12-23 01:00:13 +0700

    First Commit

---
+++ src/main/java/com/ws/ti/c/ws/basic/exceptions/NonexistentEntityException.java
@@ -0,0 +1,10 @@
+package com.ws.ti.c.ws.basic.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/ws/ti/c/ws/basic/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package com.ws.ti.c.ws.basic.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
